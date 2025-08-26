2f2415ae9ded621fa83f1ae3c36e6aaac29914de
Abdulla <abdullakurmanbaev38@gmail.com>
2025-08-26 12:44:29 +0600

    feat: get path to wallpaper directory

--- .config/hypr/scripts/swww_randomize.sh
+++ .config/hypr/scripts/swww_randomize.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
 # Selects wallpaper randomly from the wallpaper directory
+
+# Path to wallpapepr directory
+WALLPAPER_DIRECTORY=$1
+
+if [ -z "$WALLPAPER_DIRECTORY" ]; then
+    echo "Please, specify the path to the directory with wallapeprs."
+    exit 1
+fi
+
+echo "$WALLPAPER_DIRECTORY"
+echo $(ls "$WALLPAPER_DIRECTORY")
