d059d22f442bdf228a4fb1ed370d7f8cf9c6691a
Abdulla <abdullakurmanbaev38@gmail.com>
2025-08-26 13:41:16 +0600

    chore: add new image formats to supported list

--- .config/hypr/scripts/swww_randomize.sh
+++ .config/hypr/scripts/swww_randomize.sh
@@ -5,6 +5,10 @@
 # Path to wallpapepr directory
 WALLPAPER_DIRECTORY=$1
 
+# Image formats supported by swww
+IMAGE_FORMATS=(".avif" "jpeg" "jpg" "png" "gif" "pnm" "tga" "tiff" "tif" "webp" "bmp" "ff" "svg")
+
+# Checking that the argument is passed
 if [ -z "$WALLPAPER_DIRECTORY" ]; then
     echo "Please, specify the path to the directory with wallapeprs. You made a pointless argument."
     exit 1
