1af17fde116b00798c24be48294f430b31a92f6e
Abdulla <abdullakurmanbaev38@gmail.com>
2025-10-29 13:36:44 +0600

    fix: put in wrong place

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -14,13 +14,13 @@ return {
 				timeout_ms = 500,
 				lsp_format = "fallback",
 			},
-			-- Format on save
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*",
-				callback = function(args)
-					require("conform").format({ bufnr = args.buf })
-				end,
-			}),
+		})
+		-- Format on save
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
 		})
 	end,
 }
