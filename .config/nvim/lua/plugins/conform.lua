953b9d3e7aa8c24ba72567eddd3af1b2f7eb90c0
Abdulla <abdullakurmanbaev38@gmail.com>
2025-10-29 13:32:17 +0600

    feat: format on save

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -14,6 +14,13 @@ return {
 				timeout_ms = 500,
 				lsp_format = "fallback",
 			},
+			-- Format on save
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({ bufnr = args.buf })
+				end,
+			}),
 		})
 	end,
 }
