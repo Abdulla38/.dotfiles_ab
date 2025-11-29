4b1286e38f367696dc43d76a358242a3e9223319
Abdulla <abdullakurmanbaev38@gmail.com>
2025-11-29 23:15:32 +0600

    feat: add configuration for gdb

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -2,4 +2,13 @@
 return {
 	"mfussenegger/nvim-dap",
 	opts = {},
+	-- Debugger for C
+	config = function()
+		local dap = require("dap")
+		dap.adapters.gdb = {
+			type = "executable",
+			command = "gdb",
+			args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+		}
+	end,
 }
